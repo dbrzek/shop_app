57edd20ee14083f31950ebac5364f4102e153146
Damian <brzek.damian@gmail.com>
2022-05-14 15:52:29 +0200

    Adding displaying and database of products

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  //const ProductItem({ Key? key }) : super(key: key);
+  final String id;
+  final String title;
+  final String imageUrl;
+  final double price;
+
+  ProductItem(this.id, this.title, this.imageUrl, this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        // subtitle: Text(
+        //   '$price',
+        //   textAlign: TextAlign.center,
+        // ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
